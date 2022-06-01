17a0f835a7aff14248a13856dd0c3316a54f29cd
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-06-01 11:33:18 -0300

    JPA e Repository

---
+++ TicketTraevo/src/test/java/br/com/traevo/TicketTraevo/TicketTraevoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.traevo.TicketTraevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTraevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
